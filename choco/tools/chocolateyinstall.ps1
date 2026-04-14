ecf286802f7ac3231c4a04de3f7a39e8b5733f29
Your Name <you@example.com>
2026-04-14 11:12:05 +0530

    choco: add packageSourceUrl, restore template placeholders for workflow

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsDir
-  url64bit       = 'https://github.com/psmux/pstop/releases/download/v0.5.3/pstop-windows-x86_64.zip'
-  checksum64     = '37d07b823df9b20bc07163283897f03a45851783072e921d7b30f34b42d6f284'
+  url64bit       = '__DOWNLOAD_URL__'
+  checksum64     = '__CHECKSUM__'
   checksumType64 = 'sha256'
 }
 
